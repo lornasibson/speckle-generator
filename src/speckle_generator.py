130f12510698dab752000b638664da99583f71e3
lornasibson <lorna.sibson@ukaea.uk>
2024-09-11 09:21:15 +0100

    even grid of dots with random sizes created

--- src/speckle_generator.py
+++ src/speckle_generator.py
@@ -9,8 +9,13 @@ x_values = np.linspace(0, width, x_number)
 y_values = np.linspace(0, height, y_number)
 x_coords, y_coords = np.meshgrid(x_values, y_values)
 
-dot_size = 20
-plt.plot(x_coords, y_coords, marker='o', color='k', linestyle='none', markersize=dot_size)
+#dot_size = 20
+#Making every marker a different size
+num_dots = x_number * y_number
+dot_size = np.random.randint(20, 500, num_dots)
+
+# plt.plot(x_coords, y_coords, marker='o', color='k', linestyle='none', markersize=dot_size)
+plt.scatter(x_coords, y_coords, dot_size, marker='o', color='k')
 plt.xticks([])
 plt.yticks([])
 plt.show()
