673fbe1924206bfed324ee38d58964a2e249eb6c
lornasibson <lorna.sibson@ukaea.uk>
2024-09-11 14:37:57 +0100

    trying to fix area calculator

--- src/speckle_generator.py
+++ src/speckle_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
 def even_grid():
     width = 800
@@ -27,9 +28,9 @@ def even_grid():
     plt.yticks([])
     plt.show()
 
-def dot_area(dot_size, px):
+def dot_area(num_values, px):
     area = 0
-    for dot in range(dot_size):
+    for dot in range(num_values):
         diam_pts = dot**0.5
         diam_inch = diam_pts * (1/72)
         rad_inch = diam_inch / 2
@@ -58,20 +59,33 @@ def random_speckle():
         for j in range(y_number):
             y_coords[j][i] = np.random.randint(0, height)   
 
-    dot_size = 100
-    area = dot_area(dot_size, px)
-
+    num_values = x_number * y_number
+    area = dot_area(num_values, px)
+    
 
     tot_area = width * height
+    print(tot_area)
     bw_ratio = (area / tot_area) * 100
     print('B/w ratio:', bw_ratio, '%')
 
-    # plt.plot(x_coords, y_coords, marker='o', color='k', linestyle='none', markersize=dot_size)
+    dot_size = 100
     plt.figure(figsize=(width*px, height*px))
     plt.scatter(x_coords, y_coords, dot_size, marker='o', color='k')
     plt.xticks([])
     plt.yticks([])
-    plt.show()
+    plt.savefig('speckle_pattern.png')
+    
+    
+    img = Image.open('speckle_pattern.png')
+    im_data = np.array(img)
+    h, w = im_data.shape[:2]
+    colours, counts = np.unique(im_data.reshape(-1,3), axis=0, return_counts=1)
+    for index, colour in enumerate(colours):
+        count = counts[index]
+        proportion = (100 * count) / (h * w)
+        print(type(colour))
+        #print(f"   Colour: {colour}, count: {count}, proportion: {proportion:.2f}%")
+
 
     
 #Main script
