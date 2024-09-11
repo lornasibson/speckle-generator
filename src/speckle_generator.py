981b7a7ffce0a339359fb3351a4ddadc03be28a9
lornasibson <lorna.sibson@ukaea.uk>
2024-09-11 12:11:26 +0100

    random size dots removed

--- src/speckle_generator.py
+++ src/speckle_generator.py
@@ -2,34 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def even_grid():
-    height = 40
-    width = 30
+    width = 800
+    height = 600
     x_number = 10
     y_number = 10
+    px = 1/plt.rcParams['figure.dpi'] #pixel to inch conversion
     x_values = np.linspace(0, width, x_number)
     y_values = np.linspace(0, height, y_number)
     x_coords, y_coords = np.meshgrid(x_values, y_values)
 
-    #dot_size = 20
+    dot_size = 500
     #Making every marker a different size
-    num_dots = x_number * y_number
-    dot_size = np.random.randint(20, 500, num_dots)
-    area = dot_area(dot_size)
-    print(area)
+    area = dot_area(dot_size, px)
+
+    tot_area = width * height
+    bw_ratio = (area / tot_area) * 100
+    print('B/w ratio:', bw_ratio, '%')
 
     # plt.plot(x_coords, y_coords, marker='o', color='k', linestyle='none', markersize=dot_size)
+    plt.figure(figsize=(width*px, height*px))
     plt.scatter(x_coords, y_coords, dot_size, marker='o', color='k')
     plt.xticks([])
     plt.yticks([])
     plt.show()
 
-def dot_area(dot_size):
+def dot_area(dot_size, px):
     area = 0
-    for dot in dot_size:
+    for dot in range(dot_size):
         diam_pts = dot**0.5
         diam_inch = diam_pts * (1/72)
         rad_inch = diam_inch / 2
-        dot_area = np.pi * (rad_inch**2)
+        rad_px = rad_inch / px
+        dot_area = np.pi * (rad_px**2)
         area += dot_area
     return area
 
