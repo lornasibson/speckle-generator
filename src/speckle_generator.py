340835b4c9aa287430de32b3fd0690a8bb2ae971
lornasibson <lorna.sibson@ukaea.uk>
2024-09-12 14:56:31 +0100

    muDIC speckle method added

--- src/speckle_generator.py
+++ src/speckle_generator.py
@@ -97,7 +97,8 @@ def array_speckle():
     px = 1/plt.rcParams['figure.dpi'] #pixel to inch conversion
     size_x = 800
     size_y = 600
-    num_dots = 100
+    print(size_x*px, size_y*px)
+    num_dots = 600
     proportion = 0
     proportion_goal = 50
     radius = 10
@@ -105,7 +106,7 @@ def array_speckle():
     plt.figure(figsize=(size_x*px, size_y*px))
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(image, cmap='gray')
+    plt.imshow(image, cmap='grey')
     fig = plt.gcf()
     ax = fig.gca()
     plt.gca().set_axis_off()
@@ -128,31 +129,75 @@ def array_speckle():
     io_buf.seek(0)
     img_arr = np.reshape(np.frombuffer(io_buf.getvalue(), dtype=np.uint8),
                         newshape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1))
-    threshold = 150
-    img_binary = 255 * (img_arr > threshold)
+    # threshold = 27.5
+    # img_binary = 255 * (img_arr > threshold)
     #img_cropped = img_binary[50:650, 50:850, :]
     io_buf.close()
-    h, w = img_binary.shape[:2]
-    colours, counts = np.unique(img_binary.reshape(-1,3), axis=0, return_counts=1)
-    countsum = 0
+    h, w = img_arr.shape[:2]
+    colours, counts = np.unique(img_arr.reshape(-1,3), axis=0, return_counts=1)
+    total_prop = 0
     for index, colour in enumerate(colours):
         count = counts[index]
-        # print("Colour: ", colour, "Count: ", count)
         all_proportion = (100 * count) / (h * w)
-        white = np.array([255, 255, 255])
-        black = np.array([0, 0, 0])
-        if np.array_equal(colour, white):
-            proportion = all_proportion
-            print('White proportion: ', proportion)
-        elif np.array_equal(colour, black):
-            print('Black proportion:', all_proportion)
+        if all_proportion > 10:
+            print("Colour: ", colour, "Count: ", count, "Proportion: ", all_proportion)
+        total_prop += all_proportion
+        # white = np.array([255, 255, 255])
+        # black = np.array([0, 0, 0])
+        # if np.array_equal(colour, white):
+        #     proportion = all_proportion
+        #     print('White proportion: ', proportion)
+        # elif np.array_equal(colour, black):
+        #     print('Black proportion:', all_proportion)
 
-    print(countsum)
     plt.savefig('speckle_pattern_2', bbox_inches='tight', pad_inches=0)
+    print(total_prop)
     plt.show()
-    
+
+def muDIC_speckle():
+    mpl.rcParams['savefig.pad_inches'] = 0
+    px = 1/plt.rcParams['figure.dpi'] #pixel to inch conversion
+    size_x = 800
+    size_y = 600
+    num_dots = 600
+    proportion = 0
+    proportion_goal = 50
+    radius = 10
+    image = np.zeros((size_y, size_x))
+    # Add circle
+    circle = np.zeros((radius * 2, radius * 2))
+    xs, ys = np.meshgrid(np.arange(2 * radius), np.arange(2* radius))
+    r = ((xs - radius) ** 2 + (ys - radius) ** 2)** 0.5
+    circle[r < radius] = 1
+    count = 0
+
+    for i in range(num_dots):
+        pos_x = np.random.randint(0, size_x)
+        pos_y = np.random.randint(0, size_y)
+
+        image[pos_x-radius:pos_x+radius, pos_y-radius:pos_y+radius] += circle
+        count += 1
+        print(count)
+
+    plt.figure(figsize=(size_x*px, size_y*px))
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(image, cmap='grey')
+    plt.show()
+    # fig = plt.gcf()
+    # ax = fig.gca()
+    # plt.gca().set_axis_off()
+    # plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    # plt.margins(0,0)
+    # plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    # plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    # for i in range(num_dots):
+        
+    #     circ = plt.Circle((pos_x, pos_y), radius, color='w')
+    #     ax.add_patch(circ)
+
 
 #Main script
 
-array_speckle()
+muDIC_speckle()
 
