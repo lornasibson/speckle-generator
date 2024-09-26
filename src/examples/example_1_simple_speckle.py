9ac34a1bbb4653e721ad374f5c412c73e110fdbf
lornasibson <lorna.sibson@ukaea.uk>
2024-09-26 08:52:20 +0100

    function calls changed to reflect new placement of variables

--- src/examples/example_1_simple_speckle.py
+++ src/examples/example_1_simple_speckle.py
@@ -12,15 +12,13 @@ def main() -> None:
     - Image displayed on screen
     - Image saved to specifed filename in specified location
     '''
-
     filename = 'speckle_pattern_set0'
-    file_format = 'tiff'
     directory = Path.cwd() / "images"
     speckle_data = SpeckleData()
-    speckle = Speckle(filename, file_format, directory, speckle_data)
+    speckle = Speckle(speckle_data)
     image = speckle.generate_speckle()
     show_image(image)
-    save_image(image)
+    save_image(image, directory, filename)
 
 if __name__ == '__main__':
     main()
