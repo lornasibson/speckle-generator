10146fd4b63d6c1ce3874c62928c1a694305ac64
lornasibson <lorna.sibson@ukaea.uk>
2024-10-01 11:15:46 +0100

    regression test added

---
+++ tests/test_regression.py
@@ -0,0 +1,24 @@
+import pytest
+from PIL import Image
+import numpy as np
+from specklegenerator.specklegenerator import Speckle, SpeckleData, FileFormat
+
+@pytest.fixture(autouse=True)
+def setup_teardown():
+    # Setup
+    yield
+    # Teardown - remove output files
+
+
+def test_happy_speckle():
+    seed = 8
+    SpeckleData(size_x=100, size_y=100, radius=5, proportion_goal=0.5, white_bg=True,
+                image_res=200, file_format=FileFormat.TIFF.value, bits=8)
+    speckler = Speckle(SpeckleData, seed)
+    image = speckler.make()
+
+    # Load ref image
+    ref_image = Image.open('/home/lorna/speckle_generator/images/regression_image_100.tiff')
+    ref_image_array = np.asarray(ref_image)
+
+    assert image == pytest.approx(ref_image_array)
