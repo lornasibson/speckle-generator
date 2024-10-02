9b9ecc46c34fdd63ad9c53ff57cbf9770d51d7c9
lornasibson <lorna.sibson@ukaea.uk>
2024-10-02 13:26:42 +0100

    formatting corrected

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -6,9 +6,10 @@ from specklegenerator.specklegenerator import Speckle, SpeckleData, FileFormat
 
 def test_happy_speckle():
     seed = 8
-    SpeckleData(size_x=100, size_y=100, radius=5, proportion_goal=0.5, white_bg=True,
-                image_res=200, file_format=FileFormat.TIFF.value, bits=8)
-    speckler = Speckle(SpeckleData, seed)
+    data = SpeckleData(size_x=100, size_y=100, radius=5, proportion_goal=0.5,
+                white_bg=True, image_res=200, file_format=FileFormat.TIFF.value,
+                  bits=8)
+    speckler = Speckle(data, seed)
     image = speckler.make()
 
     # Load ref image
