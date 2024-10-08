44ebc50a4cd63ab5c51af9f6edc8c7aceb12ae6c
lornasibson <lorna.sibson@ukaea.uk>
2024-10-08 10:00:57 +0100

    file formatted using ruff

--- src/examples/example_1_simple_speckle.py
+++ src/examples/example_1_simple_speckle.py
@@ -1,28 +1,28 @@
-'''
+"""
 example: generating a simple speckle pattern
-'''
+"""
+
 from pathlib import Path
 from specklegenerator.specklegenerator import (
     Speckle,
     SpeckleData,
-    _px_locations,
     show_image,
     save_image,
-    mean_intensity_gradient)
+)
+# mean_intensity_gradient)
+
 
 def main() -> None:
-    '''
+    """
     Speckle example: generate simple speckle pattern
     - Size of image can be specified
     - Radius of circle and the b/w ratio can be specified
     - Image displayed on screen
     - Image saved to specifed filename in specified location
-    '''
-    filename = 'speckle_large'
+    """
+    filename = "speckle_large"
     directory = Path.cwd() / "images"
     speckle_data = SpeckleData()
-
-
 
     speckle = Speckle(speckle_data)
     image = speckle.make()
@@ -30,5 +30,6 @@ def main() -> None:
     save_image(image, directory, filename)
     # mean_intensity_gradient(image)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
