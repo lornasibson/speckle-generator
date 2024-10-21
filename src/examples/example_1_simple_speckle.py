cdbbef26873cae36dcaead77d7ecc3d506ef885a
lornasibson <lorna.sibson@ukaea.uk>
2024-10-21 09:08:57 +0100

    file changed to test new changes

--- src/examples/example_1_simple_speckle.py
+++ src/examples/example_1_simple_speckle.py
@@ -3,14 +3,13 @@ example: generating a simple speckle pattern
 """
 
 from pathlib import Path
-import numpy as np
 from specklegenerator.specklegenerator import (
     Speckle,
     SpeckleData,
+    FileFormat,
     show_image,
     save_image,
     mean_intensity_gradient,
-    validate_speckle_data,
 )
 
 
@@ -22,21 +21,23 @@ def main() -> None:
     - Image displayed on screen
     - Image saved to specifed filename in specified location
     """
-    filename = "speckle_new_threshold"
+    filename = "test_image_res_300"
     directory = Path.cwd() / "images"
-    speckle_data = SpeckleData(size_x=400,
-                               size_y=400,
-                               radius=7,
+    speckle_data = SpeckleData(size_x=600,
+                               size_y=600,
+                               radius=5,
                                b_w_ratio=0.5,
                                white_bg=True,
                                bits=8,
                                )
 
-    speckle = Speckle(speckle_data)
+    speckle = Speckle(speckle_data, seed=8)
     image = speckle.make()
     print(f"{mean_intensity_gradient(image)=}")
     show_image(image)
-    save_image(image, directory, filename, speckle_data.bits)
+    # save_image(image, directory, filename, speckle_data.bits, speckle_data.file_format,
+            #    speckle_data.image_res)
+
 
 
 if __name__ == "__main__":
